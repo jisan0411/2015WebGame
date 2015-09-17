6bbe5d16d1cca95bb4ba3d8de09003079e900a33
jisan0411 <kingjisan@naver.com>
2015-09-17 21:38:27 +0900

    20150917

--- Canvas.js
+++ Canvas.js
@@ -3,6 +3,110 @@
 function drawScreen(){
 	var theCanvas = document.getElementById("GameCanvas");
 	var Context = theCanvas.getContext("2d");
-	Context.fillSytle = "#000000";
+
+	Context.fillStyle = "#505000";
 	Context.fillRect(0,0,1000,700);
+
+	//달
+
+	Context.beginPath();
+	Context.arc(800,130,150,1*Math.PI,3*Math.PI,false);
+	Context.fillStyle="#ff0";
+	Context.fill();
+
+	// 1인
+
+	Context.beginPath();
+	Context.arc(400,400,50,1*Math.PI,3*Math.PI,false);
+	Context.fillStyle="#ff0";
+	Context.fill();
+
+	Context.beginPath();
+	Context.moveTo(400,450);
+	Context.lineTo(400,550);
+	Context.strokeStyle = "#fff";
+	Context.stroke();
+
+	Context.beginPath();
+	Context.moveTo(300,480);
+	Context.lineTo(500,480);
+	Context.strokeStyle = "#fff";
+	Context.stroke();
+
+	Context.beginPath();
+	Context.moveTo(400,550);
+	Context.lineTo(300,650);
+	Context.strokeStyle = "#fff";
+	Context.stroke();
+
+	Context.beginPath();
+	Context.moveTo(400,550);
+	Context.lineTo(500,650);
+	Context.strokeStyle = "#fff";
+	Context.stroke();
+
+	// 2인
+
+	Context.beginPath();
+	Context.arc(150,400,50,1*Math.PI,3*Math.PI,false);
+	Context.fillStyle="#ff0";
+	Context.fill();
+
+	Context.beginPath();
+	Context.moveTo(150,450);
+	Context.lineTo(150,550);
+	Context.strokeStyle = "#fff";
+	Context.stroke();
+
+	Context.beginPath();
+	Context.moveTo(50,480);
+	Context.lineTo(250,480);
+	Context.strokeStyle = "#fff";
+	Context.stroke();
+
+	Context.beginPath();
+	Context.moveTo(150,550);
+	Context.lineTo(50,650);
+	Context.strokeStyle = "#fff";
+	Context.stroke();
+
+	Context.beginPath();
+	Context.moveTo(150,550);
+	Context.lineTo(250,650);
+	Context.strokeStyle = "#fff";
+	Context.stroke();
+
+	//
+	
+	Context.beginPath();
+	Context.moveTo(150,100);
+	Context.lineTo(100,150);
+	Context.strokeStyle = "#fff";
+	Context.stroke();
+
+	Context.beginPath();
+	Context.moveTo(150,100);
+	Context.lineTo(200,150);
+	Context.strokeStyle = "#fff";
+	Context.stroke();
+
+	Context.beginPath();
+	Context.moveTo(280,100);
+	Context.lineTo(230,150);
+	Context.strokeStyle = "#fff";
+	Context.stroke();
+
+	Context.beginPath();
+	Context.moveTo(280,100);
+	Context.lineTo(330,150);
+	Context.strokeStyle = "#fff";
+	Context.stroke();
+
+	Context.beginPath();
+	Context.arc(210,220,60,1*Math.PI,2*Math.PI,true);
+	Context.fillStyle="#ff0";
+	Context.fill();
 }
+
+
+
---
+++ textdraw.js
@@ -0,0 +1,30 @@
+﻿window.addEventListener("load", drawScreen, false);
+window.addEventListener("keydown", onkeydown, false);
+window.addEventListener("keyup", onkeyup, false);
+
+var strKeyEventType = "None";
+var strKeyEventValue = "None";
+
+function drawScreen(){
+	var theCanvas = document.getElementById("GameCanvas");
+	var Context = theCanvas.getContext("2d");
+	Context.fillStyle = "#000";
+	Context.font = '24px nanumgothic';
+	Context.textBaseline = "top";
+	Context.fillText("입력된 키는 : " + strKeyEventValue, 5, 5);
+	Context.fillText("키 입력상태는 : " + strKeyEventType, 5, 30);
+}
+
+function onkeydown(e){
+	strkeyEventType = e.type;
+	if(e.keyCode)code = e.keyCode;
+	strKeyEventValue = code;
+	drawScreen();
+}
+
+function onkeyup(e){
+	strkeyEventType = e.type;
+	if(e.keyCode)code = e.keyCode;
+	strKeyEventValue = code;
+	drawScreen();
+}
